c463a6f5169ed531bacc3e8bc0ba931ff7cdc12d
Juan Rodríguez <juansero29@gmail.com>
2021-12-18 13:27:59 +0100

    Making solution build correctly

--- EnigmatiKreations.Framework.MVVM/Commanding/GenericCommandAsync.cs
+++ EnigmatiKreations.Framework.MVVM/Commanding/GenericCommandAsync.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Randomizer.Framework.Models.Contract;
 
 namespace Randomizer.Framework.ViewModels.Commanding
 {
